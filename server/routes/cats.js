c697ab2831756cec19ea766514f4a8c2179d219e
drdrr <tomas.sucharda13@seznam.cz>
2024-01-05 13:45:54 +0100

    e

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -1,45 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/**
- * GET ALL CATS
- * URL: /
- * Method: GET
- */
-router.get('/', (req, res) => {
-  res.send('GET ALL CATS');
-});
+const catsController = require("../Controllers/cats");
 
-/**
- * GET CAT BY ID
- * URL: http://localhost:3000/cats/idCat
- */
-router.get('/:id', (req, res) => {
-    res.send('GET CAT ' + req.params.id);
-  });
+router.get('/', catsController.getAllCats);
 
-/**
- * DELETE CAT 
- */
+router.get('/:id', catsController.getCatById);
 
-router.delete('/:id', (req, res) => {
-    res.send('DELETE CAT ' + req.params.id);
-  });
+router.delete('/:id', catsController.deleteCat);
 
-/**
- *  PUT CAT
- */  
+router.put('/:id', catsController.updateCat);
 
-router.put('/:id', (req, res) => {
-    res.send('UPDATED CAT ' + req.params.id);
-  });
-
-/**
- * POST CAT
- */
-
-router.post('/', (req, res) => {
-    res.send('CREATE CAT ');
-  });
+router.post('/', catsController.createCat);
 
 module.exports = router;
