d54db52c7ddecb8e653bd0aba6802c6d05424d94
drdrr <tomas.sucharda13@seznam.cz>
2024-01-12 13:50:40 +0100

    e

--- server/Controllers/cats.js
+++ server/Controllers/cats.js
@@ -1,3 +1,5 @@
+const Cat = require("../models/cats");
+
 exports.getAllCats = (req, res) => {
     res.send('GET ALL CATS');
 };
@@ -14,6 +16,24 @@ exports.updateCat = (req, res) => {
    res.send('UPDATED CAT ' + req.params.id);
 };
 
-exports.createCat = (req, res) => {
-  res.send('CREATED CAT');
+exports.createCat = async (req, res) => {
+  try {
+    const data = new Cat({
+      name: req.body.name,
+      legs: req.body.legs,
+      color: req.body.color,
+    });
+    const result = await data.save();
+    if(result) {
+      return res.status(201).send({
+        msg: "Cat created",
+        payload: result
+      });
+    }
+    res.status(500).send({
+      msg:"Cat was not created"
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catsRouter = require('./routes/cats');
+const mongoose = require('mongoose');
+mongoose
+.connect('mongodb+srv://admin:adminadmin@cluster0.hsbrndg.mongodb.net/pripravamat?retryWrites=true&w=majority')
+.then(() => console.log("Database connected"))
+.catch(() => console.log(err));
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catsRouter = require('./routes/cats');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,21 +30,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 //http://localhost:3000/cats
 app.use('/cats', catsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
