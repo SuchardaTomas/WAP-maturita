50c341c4307f0770a5c6e3cea2c1ff8e3f47db11
drdrr <tomas.sucharda13@seznam.cz>
2024-02-09 13:49:43 +0100

    2024-02-09

---
+++ client/src/Models/Cat.js
@@ -0,0 +1,56 @@
+export const getAllCats = async () => {
+  const req = await fetch("http://localhost:3000/cats", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appliaction/json",
+    },
+    method: "GET",
+  });
+  const data = await req.json();
+};
+
+export const getCatById = async (id) => {
+  const req = await fetch(`http://localhost:3000/cats/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appliaction/json",
+    },
+    method: "GET",
+  });
+  const data = await req.json();
+};
+
+export const createCat = async (formData) => {
+  const req = await fetch(`http://localhost:3000/cats/`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appliaction/json",
+    },
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+  const data = await req.json();
+};
+
+export const updateCat = async (id, formData) => {
+  const req = await fetch(`http://localhost:3000/cats/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appliaction/json",
+    },
+    method: "PUT",
+    body: JSON.stringify(formData),
+  });
+  const data = await req.json();
+};
+
+export const deleteCat = async (id) => {
+  const req = await fetch(`http://localhost:3000/cats/${id}`, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "appliaction/json",
+    },
+    method: "DELETE",
+  });
+  const data = await req.json();
+};
--- client/src/Pages/MainPage/MainPage.jsx
+++ client/src/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom"
 
 export default function MainPage() {
     return(
         <>
-            <h1>Hellow World</h1>
-            <p>Hi</p>
+
+        <Link to={"/cat/aaa"}>
+            <p>Cat view</p>
+        </Link>
+
+        <Link to={"/createcat"}>
+            <p>Cat create form</p>
+        </Link>
+
+        <Link to={"/updatecat/aaa"}>
+            <p>Cat update form</p>
+        </Link>
+
+        <Link to={"/cats"}>
+            <p>Cat list</p>
+        </Link>
+            
+            
+            
+            
+
         </>
     )
 }
